84955b3697579124c51d2f319199c3d1274290e7
Amiel De Los Reyes <amieldelosreyes@Amiels-MacBook-Pro.local>
2025-04-09 09:28:20 -0600

    currently working cab driver publisher with a console consumer

--- cab-book-driver/src/main/java/com/kafkatest/cab_book_driver/controller/CabLocationController.java
+++ cab-book-driver/src/main/java/com/kafkatest/cab_book_driver/controller/CabLocationController.java
@@ -1,9 +1,10 @@
 package com.kafkatest.cab_book_driver.controller;
 
 import com.kafkatest.cab_book_driver.service.CabLocationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/location")
 public class CabLocationController {
 
@@ -19,13 +22,21 @@ public class CabLocationController {
     private CabLocationService cabLocationService;
 
     @PutMapping
-    public ResponseEntity updateLocation() {
+    public ResponseEntity<?> updateLocation() throws InterruptedException {
+        int range = 10;
 
-        int range = 100;
-        while(range > 0) {
-            System.out.println(Math.random() + "," + Math.random());
-            range--;
+        log.info("Starting location update stream for {} iterations", range);
+
+        while (range-- > 0) {
+            String location = Math.random() + "," + Math.random();
+            cabLocationService.updateLocation(location);
+
+            log.debug("Sent location update: {}", location);
+
+            Thread.sleep(1000); // 1 second interval
         }
-        return new ResponseEntity<>(Map.of("message", "Location Updated"), HttpStatus.OK);
+
+        log.info("Completed sending all location updates.");
+        return new ResponseEntity<>(Map.of("message", "Location updates completed."), HttpStatus.OK);
     }
 }
--- cab-book-driver/src/main/java/com/kafkatest/cab_book_driver/service/CabLocationService.java
+++ cab-book-driver/src/main/java/com/kafkatest/cab_book_driver/service/CabLocationService.java
@@ -1,7 +1,35 @@
 package com.kafkatest.cab_book_driver.service;
 
+import com.kafkatest.cab_book_driver.constant.AppConstant;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class CabLocationService {
+
+    /**
+     * Use Kafka Template
+     * - Helps us create the template and store the data to your Apache Kafka cluster.
+     * - Key-Value combination
+     * - Which topic we need to send the data
+     * - Mostly configuration
+     *
+     * Here, our combination is:
+     *      Key: String
+     *      Value: Object
+     */
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    public boolean updateLocation(String location) {
+        // Use kafkaTemplate then send the data (location) to Kafka Topic (cab-location)
+        log.info("Publishing cab location to Kafka topic: {}, data: {}", AppConstant.CAB_LOCATION, location);
+        kafkaTemplate.send(AppConstant.CAB_LOCATION, location);
+        return true;
+    }
 }
